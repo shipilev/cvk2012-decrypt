fa33ed1c7bce6c8f46d8d4b452218b14d9170dbb
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2012-10-23 01:41:05 +0400

    Use JDK 6 APIs.

--- src/main/java/net/shipilev/Decrypt.java
+++ src/main/java/net/shipilev/Decrypt.java
@@ -2,7 +2,9 @@ package net.shipilev;
 
 import org.jivesoftware.util.Blowfish;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
@@ -28,12 +30,11 @@ public class Decrypt
 
         String key = "J1]{g(S4#aj*%|@+<<.^RdjR)}b{].^mb,};H>2V.R6L.Kx;b[Pv~91tv)f'~Ue!";
 
-        // Should it be UTF8?
-        Charset charset = Charset.defaultCharset();
-
         // That fucking ridiculous to use 3rd party lib incompatible with Openssl
         Blowfish blowfish = new Blowfish(key);
-        String enc = new String(Files.readAllBytes(Paths.get(in)), charset);
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File(in)));
+        String enc = reader.readLine();
         String dec = blowfish.decryptString(enc);
 
         PrintWriter pw = new PrintWriter(new File(out));
